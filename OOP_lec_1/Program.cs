c69403b907b6f165562c0cb6150e9b53dc892032
grizzly0320 <91096782+grizzly0320@users.noreply.github.com>
2022-12-13 21:55:05 +0300

    Добавьте файлы проекта.

---
+++ OOP_lec_1/Program.cs
@@ -0,0 +1,23 @@
+﻿MyClass obj = new MyClass();
+obj.SomeAction();
+Console.WriteLine(obj.GetA());
+class MyClass
+{
+    /*Переменные описанные в классе называются полями, а функции - методами;
+     * поля и методы класса называются элементами класс*/
+    private int a;
+    //Конструктор по умолчанию
+    /*Конструктор - это метод класса, который используется начальной инициализации класса*/
+    public MyClass()
+    {
+        a = 1;
+    }
+    public void SomeAction()
+    {
+        a++;
+    }
+    public int GetA()
+    {
+        return a;
+    }
+}
